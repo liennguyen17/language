a660373651b0016a9d39c639d4f3f39f16052b21
liennguyen17 <nguyenlienltw@gmail.com>
2022-08-24 11:38:41 +0700

    update

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\App;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    echo $locale = App::currentLocale();
+    return view('welcome');
+});
+
+Route::get('/lang/{locale}', function ($locale) {
+    if (! in_array($locale, ['en', 'jp', 'vn'])) {
+        abort(400);
+    }
+ 
+    App::setLocale($locale);
+ 
+    Session::put('locale',$locale);
+    return redirect()->back();
+});
